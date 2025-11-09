fdd40f8bb49879c9b6c27801aeac37650bbab50d
htkelnino <ky21112005@gmail.com>
2025-11-09 19:58:57 +0700

    Add new menus
    Game Over menu and Pause menu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,9 @@
-﻿using TMPro;
+﻿using DG.Tweening.Core.Easing;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
@@ -18,12 +19,17 @@ public class PlayerController : MonoBehaviour
     private bool isGrounded;
     public float gravityValue = -9.81f;
 
-    public float speed = 0;   
+    public float speed = 0;
     public float rotationSpeed = 10f;
     private int score = 0;
 
-    private int totalCoins = 0; 
+    private int totalCoins = 0;
     private int coinsEaten = 0;
+
+    public GameObject gameOverPanel;
+
+    public GameObject pausePanel;
+    private bool isPaused = false;
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -47,6 +53,11 @@ public class PlayerController : MonoBehaviour
         if (isGrounded && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
         }
 
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
@@ -83,13 +94,17 @@ public class PlayerController : MonoBehaviour
             Destroy(collision.gameObject);
             HandleCoinEaten();
         }
+        else if (collision.CompareTag("Ghost"))
+        {
+            ShowGameOverScreen();
+        }
     }
     private void HandleCoinEaten()
     {
         coinsEaten++;
         if (coinsEaten >= totalCoins)
         {
-            ShowWinScreen(); 
+            ShowWinScreen();
         }
     }
 
@@ -113,5 +128,43 @@ public class PlayerController : MonoBehaviour
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("Main Menu");
+    }
+
+    public void LoadCurrentLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void ShowGameOverScreen()
+    {
+        if (gameOverPanel != null)
+        {
+            gameOverPanel.SetActive(true);
+
+            Time.timeScale = 0f;
+            this.enabled = false;
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+            if (pausePanel != null) pausePanel.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            if (pausePanel != null) pausePanel.SetActive(false);
+        }
+    }
+
+    public void ResumeGame()
+    {
+        TogglePause();
     }
 }
